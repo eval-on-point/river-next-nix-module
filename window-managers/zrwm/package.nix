4ebe6fcd3fa6556322cda1bf8b8119354b7695e1
David Mkhitaryan <davtot111@icloud.com>
2026-03-24 13:36:31 +0400

    Remove now redundant libxkbcommon patching

--- window-managers/zrwm/package.nix
+++ window-managers/zrwm/package.nix
@@ -8,50 +8,9 @@
   libxkbcommon,
   lib,
   git,
-  fetchFromGitHub,
-  writeText,
 }:
-
 let
   exampleConfig = ./init;
-  libxkbcommon_13 = libxkbcommon.overrideAttrs (
-    final: prev: {
-      version = "1.13.1";
-      src = fetchFromGitHub {
-        owner = "xkbcommon";
-        repo = "libxkbcommon";
-        tag = "xkbcommon-${final.version}";
-        hash = "sha256-wUsxsM0xXTg7nbvFMXrrnHherOepj0YI77eferjRgJA=";
-      };
-      patches = [
-        (writeText "disable-x11com.patch" ''
-          On nixpkgs /tmp/.X11-unix is not compatible with Xvfb requirement and the
-          test fails.
-          --- a/meson.build
-          +++ b/meson.build
-          @@ -1229,18 +1229,6 @@ if get_option('enable-x11')
-                   env: test_env,
-                   is_parallel : false,
-               )
-          -    test(
-          -        'x11comp',
-          -        executable(
-          -            'test-x11comp',
-          -            'test/x11comp.c',
-          -            'test/utils-text.c',
-          -            'test/utils-text.h',
-          -            dependencies: x11_xvfb_test_dep
-          -        ),
-          -        env: test_env,
-          -        is_parallel : false,
-          -    )
-           endif
-           if get_option('enable-xkbregistry')
-               test(
-        '')
-      ];
-    }
-  );
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "zrwm";
@@ -71,7 +30,7 @@ stdenv.mkDerivation (finalAttrs: {
   ];
   buildInputs = [
     wayland
-    libxkbcommon_13
+    libxkbcommon
     wayland-protocols
   ];
 
